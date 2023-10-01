92d4c737e65d51403c9e3d0f82da62f2155d5b85
ingridmidia <ingridmidia@gmail.com>
2023-10-01 15:36:55 -0500

    added event listerner to history buttons

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -113,16 +113,20 @@ function searchHistory(city) {
 
 function displayHistory() {
     var cities = JSON.parse(localStorage.getItem("cities")) || [];
-        for(var i = 0; i < cities.length; i++){
-            var searchHistory = document.getElementById("search-history");
-            var button = document.createElement("button");
-            button.textContent = cities[i];
-            searchHistory.classList.add("d-grid");
-            button.classList.add("btn");
-            button.classList.add("btn-secondary");
-            button.classList.add("m-1");
-            searchHistory.appendChild(button);
-        }
+    for (var i = 0; i < cities.length; i++) {
+        var searchHistory = document.getElementById("search-history");
+        var button = document.createElement("button");
+        button.textContent = cities[i];
+        searchHistory.classList.add("d-grid");
+        button.classList.add("btn");
+        button.classList.add("btn-secondary");
+        button.classList.add("m-1");
+        searchHistory.appendChild(button);
+        button.addEventListener("click", function (event) {
+            var url = "./dashboard.html?q=" + event.target.innerText;
+            location.assign(url);
+        });
+    }
 }
 
 displayHistory();
