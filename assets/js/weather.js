75b121dd5665c4d28f01c73949dbf5022d0257ee
ingridmidia <ingridmidia@gmail.com>
2023-10-01 11:20:26 -0500

    created buttons for search history

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -101,4 +101,28 @@ searchForm.addEventListener("submit", function (event) {
 
     var url = "./dashboard.html?q=" + cityInput;
     location.assign(url);
+
+    searchHistory(cityInput);
 });
+
+function searchHistory(city) {
+    var cities = JSON.parse(localStorage.getItem("cities")) || [];
+    cities.push(city);
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+function displayHistory() {
+    var cities = JSON.parse(localStorage.getItem("cities")) || [];
+        for(var i = 0; i < cities.length; i++){
+            var searchHistory = document.getElementById("search-history");
+            var button = document.createElement("button");
+            button.textContent = cities[i];
+            searchHistory.classList.add("d-grid");
+            button.classList.add("btn");
+            button.classList.add("btn-secondary");
+            button.classList.add("m-1");
+            searchHistory.appendChild(button);
+        }
+}
+
+displayHistory();
